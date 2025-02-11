1178577e82f939e940b3841f45ed94fb529fbbaf
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-11 23:51:46 +0400

    Initial Commit

---
+++ WeSplitter/WeSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitterApp.swift
+//  WeSplitter
+//
+//  Created by Harriet Mathew on 11/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplitterTests/WeSplitterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitterTests.swift
+//  WeSplitterTests
+//
+//  Created by Harriet Mathew on 11/2/2025.
+//
+
+import Testing
+@testable import WeSplitter
+
+struct WeSplitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
