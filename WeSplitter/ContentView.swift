1544bdb4c035e668e7e6a4183cf1141c14ab8ac0
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-12 14:57:12 +0400

    Added a title for the app and a picker for the number of people

--- WeSplitter/ContentView.swift
+++ WeSplitter/ContentView.swift
@@ -9,20 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var priceAmount = 0.0
-    @State private var noOfPeople = 0
-    @State private var tipPercentage = 0
+    @State private var noOfPeople = 4
+    @State private var tipPercentage = 20
     
     let tipPercentages = [0, 5, 10, 15, 20]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Enter amount: ", value: $priceAmount, format: .currency(code: Locale.current.currency?.identifier ?? "AED"))
-                    .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Enter amount:", value: $priceAmount, format: .currency(code: Locale.current.currency?.identifier ?? "AED"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $noOfPeople) {
+                        ForEach(2...100, id: \.self) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(priceAmount, format:.currency(code: Locale.current.currency?.identifier ?? "AED"))
+                }
             }
-            Section {
-                Text(priceAmount, format:.currency(code: Locale.current.currency?.identifier ?? "AED"))
-            }
+            .navigationTitle("WeSplitter")
         }
     }
 }
