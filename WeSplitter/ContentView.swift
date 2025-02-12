2086fd065bddb47eea26d3df3947aa13a93ef361
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-12 14:22:01 +0400

    Declared variables

--- WeSplitter/ContentView.swift
+++ WeSplitter/ContentView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {    
+struct ContentView: View {
+    @State private var priceAmount = 0.0
+    @State private var noOfPeople = 0
+    @State private var tipPercentage = 0
+    
+    let tipPercentages = [0, 5, 10, 15, 20]
+    
     var body: some View {
-        Text("Hello World!")
     }
 }
 
